8d71722fde61688610496976a759cb8e4772087f
muhammadrizkifadhil95 <rizkifadhil95@gmail.com>
2024-09-17 17:11:14 +0700

    Done

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -1,19 +1,2 @@
 package com.example.lab_week_04
 
-class CafeAdapter {
-    val TABS_FIXED = listOf(
-        R.string.starbucks_title,
-        R.string.janjijiwa_title,
-        R.string.kopikenangan_title,
-    )
-    class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
-        FragmentStateAdapter(fragmentManager, lifecycle) {
-        override fun getItemCount(): Int {
-            return TABS_FIXED.size
-        }
-        override fun createFragment(position: Int): Fragment
-        {
-            return CafeDetailFragment()
-        }
-    }
-}
